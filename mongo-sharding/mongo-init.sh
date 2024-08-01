8a2aaaaa068caacaa3d64e2ffd12e0475e40d322
maxbolgarin <maxbolgarin@yandex.ru>
2024-08-01 11:50:20 +0300

    fix: init script

--- mongo-sharding/mongo-init.sh
+++ mongo-sharding/mongo-init.sh
@@ -2,22 +2,26 @@
 
 docker exec -t configSrv mongosh <<EOF
 rs.initiate({_id:"config_server",configsvr:true,members:[{_id:0,host:"configSrv:27017"}]});
+exit;
 EOF
 
 docker exec -t shard1 mongosh <<EOF
 rs.initiate({_id:"shard1",members:[{_id:0,host:"shard1:27018"}]});
+exit;
 EOF
 
 docker exec -t shard2 mongosh <<EOF
 rs.initiate({_id:"shard2",members:[{_id:0,host:"shard2:27019"}]});
+exit;
 EOF
 
 docker exec -t configSrv mongosh <<EOF
 sh.addShard( "shard1/shard1:27018");
 sh.addShard( "shard2/shard2:27019");
 sh.enableSharding("somedb");
-sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
-use somedb
-for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } );
+use somedb;
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i});
+exit;
 EOF
 
