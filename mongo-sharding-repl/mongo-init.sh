4ff4d7a4d45c11221308c3a694168a4cfb53451a
maxbolgarin <maxbolgarin@yandex.ru>
2024-08-01 14:38:55 +0300

    feat: upd

--- mongo-sharding-repl/mongo-init.sh
+++ mongo-sharding-repl/mongo-init.sh
@@ -5,7 +5,7 @@ rs.initiate({_id:"config_server",configsvr:true,members:[{_id:0,host:"configSrv:
 EOF
 
 docker compose exec -T shard1-1 mongosh <<EOF
-rs.initiate({_id:"shard1",version:1,members:[{_id:0,host:"shard1-1:27017",_id:1,host:"shard1-2:27017",_id:2,host:"shard1-3:27017"}]})
+rs.initiate({_id:"shard1",version:1,members:[{_id:0,host:"shard1-1:27711",_id:1,host:"shard1-2:27712",_id:2,host:"shard1-3:27713"}]})
 EOF
 
 docker compose exec -T shard2-1 mongosh <<EOF
