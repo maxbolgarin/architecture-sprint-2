3074c77ca6ee023edccb6559759fa1c6aaceb9bd
maxbolgarin <maxbolgarin@yandex.ru>
2024-08-01 11:57:23 +0300

    fix: init script

--- mongo-sharding/mongo-init.sh
+++ mongo-sharding/mongo-init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-docker exec -T configSrv mongosh <<EOF
+docker compose exec -T configSrv mongosh <<EOF
 rs.initiate({_id:"config_server",configsvr:true,members:[{_id:0,host:"configSrv:27017"}]})
 EOF
 
-docker exec -T shard1 mongosh <<EOF
+docker compose exec -T shard1 mongosh <<EOF
 rs.initiate({_id:"shard1",members:[{_id:0,host:"shard1:27018"}]})
 EOF
 
-docker exec -T shard2 mongosh <<EOF
+docker compose exec -T shard2 mongosh <<EOF
 rs.initiate({_id:"shard2",members:[{_id:0,host:"shard2:27019"}]})
 EOF
 
-docker exec -T configSrv mongosh <<EOF
+docker compose exec -T configSrv mongosh <<EOF
 sh.addShard( "shard1/shard1:27018")
 sh.addShard( "shard2/shard2:27019")
 sh.enableSharding("somedb")
