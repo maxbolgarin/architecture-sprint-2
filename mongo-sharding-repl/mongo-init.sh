7a78e703162f910373d48ac6e27eb5dcc4417e11
maxbolgarin <maxbolgarin@yandex.ru>
2024-08-01 13:43:57 +0300

    fix: shards

--- mongo-sharding-repl/mongo-init.sh
+++ mongo-sharding-repl/mongo-init.sh
@@ -5,11 +5,11 @@ rs.initiate({_id:"config_server",configsvr:true,members:[{_id:0,host:"configSrv:
 EOF
 
 docker compose exec -T shard1-1 mongosh --port 27011 <<EOF
-rs.initiate({_id:"shard1",members:[{_id:0,host:"shard1-1:27011", _id:1,host:"shard1-2:27012", _id:2,host:"shard1-3:27013"}]})
+rs.initiate({_id:"shard1",members:[{_id:0,host:"shard1-1:27011",_id:1,host:"shard1-2:27012",_id:2,host:"shard1-3:27013"}]})
 EOF
 
 docker compose exec -T shard2-1 mongosh --port 27021 <<EOF
-rs.initiate({_id:"shard2",members:[{_id:0,host:"shard2-1:27021", _id:1,host:"shard2-2:27022", _id:2,host:"shard2-3:27023"}]})
+rs.initiate({_id:"shard2",members:[{_id:0,host:"shard2-1:27021",_id:1,host:"shard2-2:27022",_id:2,host:"shard2-3:27023"}]})
 EOF
 
 docker compose exec -T mongos_router mongosh --port 27017 <<EOF
